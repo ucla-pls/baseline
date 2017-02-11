8095a6021d4e8cc8ca2d469f70fef6e28915df7c
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-02-11 12:33:16 -0800

    Added Infinite

---
+++ src/edu/ucla/pls/baseline/Infinite.java
@@ -0,0 +1,20 @@
+package edu.ucla.pls.baseline;
+/**
+ * Infinit is a simple example of a non-terminating program.
+ *
+ * @author Christian Gram Kalhauge
+ */
+public class Infinite {
+  public static void main (String[] args) {
+    Infinite inf = new Infinite();
+    synchronized (inf) {
+      try {
+        while (true) {
+          inf.wait();
+        }
+      } catch (InterruptedException e) {
+        System.out.println("Infinite loop interrupted.");
+      }
+    }
+  }
+}
