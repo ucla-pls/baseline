f67a777cd5e7363f6e30433bb1d2d99bb88267a8
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-07-16 10:57:54 +0200

    Added new test

---
+++ src/edu/ucla/pls/baseline/ReflectionReachability.java
@@ -0,0 +1,21 @@
+package edu.ucla.pls.baseline;
+/** This class tests reachabilitiy analyses, on programs that 
+ * uses reflection. 
+ */
+import java.lang.reflect.Method;
+public class ReflectionReachability {
+    public static void main(String[] args) throws Exception {
+        Class<?> c = ReflectionReachability.class;
+        Method m = c.getMethod("missed");
+        m.invoke(null);
+    }
+
+    public static void missed() { 
+        System.out.println("reached \"missed\""); 
+    };
+    
+    public static void unreachable() { 
+        // This should not happen.
+        System.out.println("reached \"unreachable\""); 
+    };
+}
